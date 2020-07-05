b357889af29a8143b0801b0301b21b6a7d8c8c66
raul.llerena.contractor <raul.llerena.contractor@bbva.com>
2020-07-04 23:37:35 -0500

    vcambios

--- src/com/rllerena/Gota.java
+++ src/com/rllerena/Gota.java
@@ -4,11 +4,61 @@ package com.rllerena;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
+import java.util.Random;
 
 public class Gota extends JComponent {
+    private Thread anima;
+//    private int x1;
+//    private int y1;
+    int radio=10;     	//radio de la pelota
+    int x, y;       	//posición del centro de la pelota
+    int dx = 100;     	//desplazamientos
+    int dy = 100;
+    int anchoApplet;
+    int altoApplet;
+
+    public void start() {
+        if (anima == null) {
+            anima = new Thread();
+            anima.start();
+        }
+    }
+
+    public void stop() {
+        if (anima != null) {
+            anima.stop();
+            anima = null;
+        }
+    }
+
+    public void run() {
+        while (true) {
+            mover();
+        }
+    }
+
+    void mover() {
+        x += dx;
+        y += dy;
+        if (x >= (anchoApplet - radio) || x <= radio) dx *= -1;
+        if (y >= (altoApplet - radio) || y <= radio) dy *= -1;
+        repaint();      //llama a update
+    }
 
     public void paint(Graphics g) {
-        g.drawLine(530, 100, 80, 90);
-//        g.drawLine(30, 20, 80, 90);
+        g.setColor(Color.red);
+        g.fillOval(x - radio, y - radio, 2 * radio, 2 * radio);
     }
+
+   /* public void paint(Graphics g) {
+        System.out.println("getWidth() {}" + getWidth());
+        System.out.println("getHeight() {}" + getHeight());
+        x1 = getWidth() / 2;
+        y1 = 90;
+        g.setColor(Color.magenta);
+
+        g.drawLine(x1, y1, x1, y1 + 10);
+
+    }*/
+
 }
